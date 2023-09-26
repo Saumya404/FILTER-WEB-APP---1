adfde023e81510abac0a43171a1473c770b0aa76
Saumya404 <107306018+Saumya404@users.noreply.github.com>
2023-09-26 18:56:51 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,15 @@
+function preload(){
+
+}
+
+function setup(){
+    canvas=createCanvas(450,400);
+    canvas.center();
+}
+
+function draw(){
+
+}
+function take_snapshot(){
+    save('Pixel_Filter.jpg');
+}
